8b4afeae6b0e8190dbe589952dd99ba80551d351
Thiago Leonardo <thiagoleocosta@gmail.com>
2019-04-07 13:02:20 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import middleware from './middleware'
+import reducer from './reducers'
+
+const store = createStore(reducer, middleware)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));
